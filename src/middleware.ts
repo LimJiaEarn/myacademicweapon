5d56dae12390313ef609c920832faa0c133a52b3
Jia Earn <jiae4rn@gmail.com>
2024-03-10 21:17:20 +0800

    Webhook Fix Attempt #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export default authMiddleware({
 
   // Routes that can be accessed while signed out
-  publicRoutes: ["/api/webhooks(.*)", '/', '/sign-in', '/study-resources','/study-resources/(.*)'],
+  publicRoutes: ["/api/webhooks/clerk", '/', '/sign-in', '/study-resources','/study-resources/(.*)'],
 
   // Routes that can always be accessed, and have
   // no authentication information
