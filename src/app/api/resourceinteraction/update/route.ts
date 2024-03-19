931c00ee5c63df77b2ee057f0d6a3d69f08b16ca
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2024-03-19 17:05:44 +0800

    Fixed route.ts handler

--- src/app/api/resourceinteraction/update/route.ts
+++ src/app/api/resourceinteraction/update/route.ts
@@ -1,24 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { updateStatusStudyResource } from '@/lib/actions/resourceinteraction.actions'; // Adjust the import path as necessary
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        try {
-            // Type the entire req.body and then destructure
-            const { userID, resourceID, status }: updateStatusStudyResourceParams = req.body;
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        // Type the entire req.body and then destructure
+        const { userID, resourceID, status }: updateStatusStudyResourceParams = req.body;
 
-            // Call the updateStatusStudyResource function with the extracted data
-            await updateStatusStudyResource({ userID, resourceID, status });
+        // Call the updateStatusStudyResource function with the extracted data
+        await updateStatusStudyResource({ userID, resourceID, status });
 
-            // Send a success response
-            res.status(200).json({ message: 'Resource status updated successfully' });
-        } catch (error) {
-            console.error('Failed to update resource status:', error);
-            res.status(500).json({ error: 'Failed to update resource status' });
-        }
-    } else {
-        // Handle any non-POST requests
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
+        // Send a success response
+        res.status(200).json({ message: 'Resource status updated successfully' });
+    } catch (error) {
+        console.error('Failed to update resource status:', error);
+        res.status(500).json({ error: 'Failed to update resource status' });
     }
+
 }
