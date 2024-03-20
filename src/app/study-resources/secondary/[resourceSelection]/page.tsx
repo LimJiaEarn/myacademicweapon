5b5d44f688869f7943f15e8519f9d43307666297
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2024-03-20 12:49:26 +0800

    Template for URL States

--- src/app/study-resources/secondary/[resourceSelection]/page.tsx
+++ src/app/study-resources/secondary/[resourceSelection]/page.tsx
@@ -1,8 +1,33 @@
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import Link from "next/link";
+
+
+
+// Tutorial Referenced: https://www.youtube.com/watch?v=ukpgxEemXsk&t=6s
+const TableOfResources = ( {searchParams} : {searchParams : { [key:string]:string | undefined}} ) => {
+
+
+
+    const test1 = searchParams.test1;
+    const test2 = searchParams.test2;
+
+
+        
+    return (
+        <div className="flex_col_center gap-2 pb-[1000px]">
+            <p className="text-xl">{test1}</p>
+            <p className="text-xl">{test2}</p>
+            
+            <Link href={`?${new URLSearchParams({
+                test1:"5sdv/j&*%^",
+                test2:"2ve"
+            })}`}>
+                5, 2
+            </Link>
+
+
+        </div>
+    )
 }
 
-export default page
+export default TableOfResources
