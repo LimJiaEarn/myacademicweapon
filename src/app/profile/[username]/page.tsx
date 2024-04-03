540e2cb4251d06931db9247242036cf26d84b2ba
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2024-04-03 16:42:28 +0800

    New Profile Page Logic

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import { useState, useEffect } from 'react';
 import { useUser, SignOutButton } from "@clerk/nextjs";
 import { getUserByUsername, getUserByClerkId } from '@/lib/actions/user.actions';
 import { getAllUserActivities } from '@/lib/actions/useractivity.actions';
@@ -11,79 +11,112 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
 
     const { user } = useUser();
     const userId = user?.id || null;
-    const { username } = params;
-
-    const currentUserProfileObject : UserObject= await getUserByUsername(username);
-    const currentSignedInUserObject : UserObject = userId ? await getUserByClerkId(userId) : null;
-    const userID = currentUserProfileObject._id; // this is the mongoDB id
-    const isOwnUser : boolean = currentSignedInUserObject && currentSignedInUserObject._id === currentUserProfileObject._id;
-
-    console.log(`auth: ${userId ? userId : "No userId"}`);
-    console.log(`MongoDb userID: ${userID}`);
-    console.log(`isOwnerUser: ${isOwnUser}`);
-
-    // Initiate the promises without awaiting them
-    const currentUserProfileTopicalData = await getAllUserActivities({ userID: currentUserProfileObject._id, resourceType: "Topical" });
-    const currentUserProfileYearlyData = await getAllUserActivities({ userID: currentUserProfileObject._id, resourceType: "Yearly" });
 
 
-    console.table(currentUserProfileTopicalData);
-    console.table(currentUserProfileYearlyData);
-
-    // Use Promise.all to await both promises in parallel
-    // const [currentUserProfileTopicalData, currentUserProfileYearlyData] = await Promise.all([
-    //     currentUserProfileTopicalDataPromise,
-    //     currentUserProfileYearlyDataPromise
-    // ]);
-
-
-    const completedResourceIDs : string[] = [...currentUserProfileTopicalData.completed, ...currentUserProfileYearlyData.completed];
-    const bookmarkedResourceIDs : string[] = [...currentUserProfileTopicalData.bookmarked, ...currentUserProfileYearlyData.bookmarked];
-    
-    const bookmarkedResourceObjectPromises = bookmarkedResourceIDs.map(async (resourceId) => {
-        return getStudyResourceByID(resourceId);
-    });
-
-    const completedResourceObjectPromises = completedResourceIDs.map(async (resourceId) => {
-        return getStudyResourceByID(resourceId);
-    });
-    
-
-    const bookmarkedResourceObjects = (await Promise.all(bookmarkedResourceObjectPromises)).filter(obj => obj !== null);
-    const completedResourceObjects = (await Promise.all(completedResourceObjectPromises)).filter(obj => obj !== null);
-
-
-    const simplifyResourceObject = (resourceObject : PracticePaperInterface) => {
-        if (!resourceObject) return null;
-
-        if (resourceObject.type==="Yearly" && 'year' in resourceObject && 'assessment' in resourceObject && 'schoolName' in resourceObject && 'paper' in resourceObject && 'subject' in resourceObject)
-            return {
-                _id: resourceObject._id.toString(),
-                status: true,
-                bookmark: true,
-                subject: resourceObject.subject,
-                title : resourceObject.subject + " " + resourceObject.year + " " + resourceObject.schoolName + " " + resourceObject.assessment + " P" + resourceObject.paper,
-                url : resourceObject.url,
-                ...(resourceObject.workingSolution && { workingSolution: resourceObject.workingSolution}),
-                ...(resourceObject.videoSolution && { videoSolution: resourceObject.videoSolution}), 
-            }
-        else if (resourceObject.type==="Topical" && 'topicName' in resourceObject && 'subject' in resourceObject)
-            return {
-                _id: resourceObject._id.toString(),
-                status: true,
-                bookmark: true,
-                subject: resourceObject.subject,
-                title : resourceObject.subject + " " + resourceObject.topicName,
-                url : resourceObject.url,
-                ...(resourceObject.workingSolution && { workingSolution: resourceObject.workingSolution}),
-                ...(resourceObject.videoSolution && { videoSolution: resourceObject.videoSolution}), 
-            }
-
-        return null;
+    if (!userId){
+        return <p>Invalid User</p>
     }
 
-    const simplifiedCompletedResourceObjects = (completedResourceObjects.map(simplifyResourceObject as any).filter(obj => obj !== null)  as ISummarisedPracticePaper[]);
-    const simplifiedBookmarkedResourceObjects = (bookmarkedResourceObjects.map(simplifyResourceObject as any).filter(obj => obj !== null)  as ISummarisedPracticePaper[]);
+    const { username } = params;
+    const [currentUserProfileObject, setcurrentUserProfileObject] = useState<UserObject>();
+    const [simplifiedCompletedResourceObjects, setSimplifiedCompletedResourceObjects] = useState<ISummarisedPracticePaper[]>([]);
+    const [simplifiedBookmarkedResourceObjects, setSimplifiedBookmarkedResourceObjects] = useState<ISummarisedPracticePaper[]>([]);
+    const [isOwnUser, setIsOwnUser] = useState(false);
+
+    console.log(`useUser: ${userId ? userId : "No userId"}`);
+    console.log(`isOwnerUser: ${isOwnUser}`);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const profile = await getUserByUsername(username);
+          setcurrentUserProfileObject(profile);
+    
+          if (userId) {
+            const signedInUser = await getUserByClerkId(userId);
+            setIsOwnUser(signedInUser?._id === profile?._id);
+          }
+        // Initiate the promises without awaiting them
+
+    if (currentUserProfileObject){
+        const currentUserProfileTopicalData = await getAllUserActivities({ userID: currentUserProfileObject._id, resourceType: "Topical" });
+        const currentUserProfileYearlyData = await getAllUserActivities({ userID: currentUserProfileObject._id, resourceType: "Yearly" });
+    
+        // Use Promise.all to await both promises in parallel
+        // const [currentUserProfileTopicalData, currentUserProfileYearlyData] = await Promise.all([
+        //     currentUserProfileTopicalDataPromise,
+        //     currentUserProfileYearlyDataPromise
+        // ]);
+    
+    
+        const completedResourceIDs : string[] = [...currentUserProfileTopicalData.completed, ...currentUserProfileYearlyData.completed];
+        const bookmarkedResourceIDs : string[] = [...currentUserProfileTopicalData.bookmarked, ...currentUserProfileYearlyData.bookmarked];
+        
+        const bookmarkedResourceObjectPromises = bookmarkedResourceIDs.map(async (resourceId) => {
+            return getStudyResourceByID(resourceId);
+        });
+    
+        const completedResourceObjectPromises = completedResourceIDs.map(async (resourceId) => {
+            return getStudyResourceByID(resourceId);
+        });
+        
+    
+        const bookmarkedResourceObjects = (await Promise.all(bookmarkedResourceObjectPromises)).filter(obj => obj !== null);
+        const completedResourceObjects = (await Promise.all(completedResourceObjectPromises)).filter(obj => obj !== null);
+    
+    
+        const simplifyResourceObject = (resourceObject : PracticePaperInterface) => {
+            if (!resourceObject) return null;
+    
+            if (resourceObject.type==="Yearly" && 'year' in resourceObject && 'assessment' in resourceObject && 'schoolName' in resourceObject && 'paper' in resourceObject && 'subject' in resourceObject)
+                return {
+                    _id: resourceObject._id.toString(),
+                    status: true,
+                    bookmark: true,
+                    subject: resourceObject.subject,
+                    title : resourceObject.subject + " " + resourceObject.year + " " + resourceObject.schoolName + " " + resourceObject.assessment + " P" + resourceObject.paper,
+                    url : resourceObject.url,
+                    ...(resourceObject.workingSolution && { workingSolution: resourceObject.workingSolution}),
+                    ...(resourceObject.videoSolution && { videoSolution: resourceObject.videoSolution}), 
+                }
+            else if (resourceObject.type==="Topical" && 'topicName' in resourceObject && 'subject' in resourceObject)
+                return {
+                    _id: resourceObject._id.toString(),
+                    status: true,
+                    bookmark: true,
+                    subject: resourceObject.subject,
+                    title : resourceObject.subject + " " + resourceObject.topicName,
+                    url : resourceObject.url,
+                    ...(resourceObject.workingSolution && { workingSolution: resourceObject.workingSolution}),
+                    ...(resourceObject.videoSolution && { videoSolution: resourceObject.videoSolution}), 
+                }
+    
+            return null;
+        }
+        const simplifiedCompletedResourceObjects = (completedResourceObjects.map(simplifyResourceObject as any).filter(obj => obj !== null)  as ISummarisedPracticePaper[]);
+        const simplifiedBookmarkedResourceObjects = (bookmarkedResourceObjects.map(simplifyResourceObject as any).filter(obj => obj !== null)  as ISummarisedPracticePaper[]);
+        
+        setSimplifiedCompletedResourceObjects(simplifiedCompletedResourceObjects);
+        setSimplifiedBookmarkedResourceObjects(simplifiedBookmarkedResourceObjects);
+
+    }
+    
+    
+
+    
+    
+
+
+
+
+
+        };
+    
+        fetchData();
+      }, [userId, username]);
+
+
+
+
 
     // To test loading
     // const delay = (ms: number) => new Promise<number>(resolve => setTimeout(() => resolve(ms), ms));
@@ -103,8 +136,8 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
                 <div className="flex_col_center gap-4">
 
                     <div className="flex_col_Center">
-                        <p className="font-bold">{currentUserProfileObject.firstName} {currentUserProfileObject.lastName}</p>
-                        <p className="italic">{currentUserProfileObject.bio}</p>
+                        <p className="font-bold">{currentUserProfileObject?.firstName} {currentUserProfileObject?.lastName}</p>
+                        <p className="italic">{currentUserProfileObject?.bio}</p>
 
                         {isOwnUser && <div>
                             <Link href={`/profile/${username}/edit`}>
@@ -123,14 +156,17 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
 
                 
             </section>
-
+            {currentUserProfileObject &&
+            <>
             <section className="w-full flex_col_center">
-                <ProfilePageTable data={simplifiedBookmarkedResourceObjects} userID={userID} sectionType="Bookmarks" isOwnUser={isOwnUser} user_name={currentUserProfileObject.firstName + currentUserProfileObject.lastName}/>
+                <ProfilePageTable data={simplifiedBookmarkedResourceObjects} userID={userId} sectionType="Bookmarks" isOwnUser={isOwnUser} user_name={currentUserProfileObject?.firstName  + currentUserProfileObject?.lastName}/>
             </section>
 
             <section className="w-full flex_col_center">
-                <ProfilePageTable data={simplifiedCompletedResourceObjects} userID={userID} sectionType="Completed" isOwnUser={isOwnUser} user_name={currentUserProfileObject.firstName + currentUserProfileObject.lastName}/>
+                <ProfilePageTable data={simplifiedCompletedResourceObjects} userID={userId} sectionType="Completed" isOwnUser={isOwnUser} user_name={currentUserProfileObject?.firstName + currentUserProfileObject?.lastName}/>
             </section>
+            </>
+            }
             
             
 
