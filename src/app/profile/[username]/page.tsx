0afec049bd9d4ac306b1e0486096490f0d1c5953
Jia Earn <jiae4rn@gmail.com>
2024-04-17 17:50:49 +0800

    More Profile Stats

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -142,96 +142,87 @@ const ProfilePage = async ({ params }: { params: { username: string } }) => {
 
   
 
-  return (
-    <div className="min-h-screen max-w-[1600px] mx-auto w-full flex flex-col lg:flex-row justify-start gap-2 md:gap-4 px-2 md:px-4 py-2">
+    return (
+        <div className="max-w-[1600px] mx-auto w-full flex flex-col lg:flex-row justify-start gap-4 md:gap-6 px-2 md:px-4 py-2">
 
-        {/* Profile Page Side Bar (lg) */}
-        <section className="bg-pri_bg_card rounded-xl w-full px-6 md:px-4 py-2 md:py-4 flex flex-col justify-start gap-4 md:gap-6 lg:max-w-[320px]">
-        
-            <div className="flex flex-col sm:flex-row lg:flex-col justify-center items-center lg:items-start gap-4 md:gap-6">
-                {/* Mini Profile Section */}
-                <div className="flex flex-row justify-start items-center gap-2 md:gap-4">
-                                
-                    <Image src={currentUserProfileObject?.photo || "/images/placeholderDP.webp"} alt="profile pic" height={90} width={90} className="rounded-lg"/>
-                    <div className="">
-                        <p className="text-lg font-bold leading-tight md:text-xl md:leading-relaxed text-pri_navy_dark">{currentUserProfileObject?.firstName} {currentUserProfileObject?.lastName}</p>
-                        <p className="text-sm italic leading-tight md:text-md md:leading-relaxed text-pri_navy_main">joined {formattedJoinDate}</p>
+            {/* Profile Page Side Bar (lg) */}
+            <section className="bg-pri_bg_card rounded-xl w-full px-6 md:px-4 py-4 md:py-6 flex flex-col justify-start gap-4 md:gap-6 lg:max-w-[340px]">
+            
+                <div className="flex flex-col sm:flex-row lg:flex-col justify-center items-center gap-4 md:gap-6">
+                    {/* Mini Profile Section */}
+                    <div className="flex flex-row justify-start items-center gap-2 md:gap-4">
+                                    
+                        <Image src={currentUserProfileObject?.photo || "/images/placeholderDP.webp"} alt="profile pic" height={90} width={90} className="rounded-lg"/>
+                        <div className="">
+                            <p className="text-lg font-bold leading-tight md:text-xl md:leading-relaxed text-pri_navy_dark">{currentUserProfileObject?.firstName} {currentUserProfileObject?.lastName}</p>
+                            <p className="text-sm italic leading-tight md:text-md md:leading-relaxed text-pri_navy_main">joined {formattedJoinDate}</p>
+                        </div>
                     </div>
-                </div>
-                
-                {/* Buttons */}
-                { isOwnUser &&
-                <div className="flex flex-row sm:flex-col lg:flex-row lg:w-full justify-center items-center gap-2 md:gap-4">
-                    <LinkButton
-                        buttonMsg="Edit Profile"
-                        buttonMsgClass="text-white text-xs md:text-sm"
-                        buttonColorClass="opacity-90 bg-teal-400 hover:bg-teal-500 border-gray-300 py-1 px-4 shadow-lg"
-                        linksTo={`/profile/${username}/edit`}
-                    />
-
-                    <SignOutButton>
+                    
+                    {/* Buttons */}
+                    { isOwnUser &&
+                    <div className="flex flex-row sm:flex-col lg:flex-row lg:w-full justify-center items-center gap-2 md:gap-4">
                         <LinkButton
-                            buttonMsg="Sign Out"
+                            buttonMsg="Edit Profile"
                             buttonMsgClass="text-white text-xs md:text-sm"
-                            // Updated to a gentle red with some opacity
-                            buttonColorClass="opacity-90 bg-rose-300 hover:bg-rose-400 border-gray-300 py-1 px-4 shadow-lg"
-                            linksTo={`/`}
+                            buttonColorClass="opacity-90 bg-teal-400 hover:bg-teal-500 border-gray-300 py-1 px-4 shadow-lg"
+                            linksTo={`/profile/${username}/edit`}
                         />
-                    </SignOutButton>
-                </div>}
-            </div>
+
+                        <SignOutButton>
+                            <LinkButton
+                                buttonMsg="Sign Out"
+                                buttonMsgClass="text-white text-xs md:text-sm"
+                                // Updated to a gentle red with some opacity
+                                buttonColorClass="opacity-90 bg-rose-300 hover:bg-rose-400 border-gray-300 py-1 px-4 shadow-lg"
+                                linksTo={`/`}
+                            />
+                        </SignOutButton>
+                    </div>}
+                </div>
 
 
 
-            <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
+                <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
 
-            <div className="flex flex-col sm:flex-row lg:flex-col justify-center items-center lg:items-start gap-4 md:gap-6">
 
                 <UserAbout isOwnUser={isOwnUser} username={username} currentUserProfileObject={currentUserProfileObject}/>
-                {/* <div className="flex_col_center">
+
+
+                <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
                 
-                    <h2 className="w-full px-2 text-md md:text-lg font-bold md:text-md text-pri_navy_dark text-start mb-2">About{isOwnUser && " You"}</h2>
-                    <p className="w-full px-2 text-sm md:text-md md:text-md text-pri_navy_main">{('school' in currentUserProfileObject) ? currentUserProfileObject.school : "No School"}</p>
-                    <p className="w-full px-2 text-sm md:text-md md:text-md text-pri_navy_main">{('level' in currentUserProfileObject) ? currentUserProfileObject.level : "No level"}</p>
-                </div> */}
+                <div className="flex flex-col sm:flex-row lg:flex-col justify-center items-center lg:items-start gap-4 md:gap-6">
 
-            </div>
-            <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
-            
-            <div className="flex flex-col sm:flex-row lg:flex-col justify-center items-center lg:items-start gap-4 md:gap-6">
+                    <p className="w-full text-center text-pri_navy_dark font-bold text-lg lg:text-xl">Don't count the days;<br className="hidden lg:flex"/> Make the days count</p>
 
-                <p className="w-full text-center text-pri_navy_dark font-bold text-lg lg:text-xl">Don't count the days;<br className="hidden lg:flex"/> Make the days count</p>
+                </div>
 
-
-            </div>
-
-            <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
-            <div className="flex justify-center w-full">
-                    
-                <Calendar/>
-
-            </div>
+                <hr className="h-0.5 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-pri_mint_darker to-transparent opacity-45" />
+                
+                <div className="flex_center w-full">        
+                    <Calendar/>
+                </div>
 
 
 
-        </section>
+            </section>
 
-    {/* Profile Page Client Components */}
-    <section className="w-full flex-grow">
+            {/* Profile Page Client Components */}
+            <section className="w-full flex-grow">
 
-        <UserProfile
-            isOwnUser={isOwnUser}
-            userID={userID}
-            simplifiedCompletedResourceObjects={simplifiedCompletedResourceObjects}
-            simplifiedBookmarkedResourceObjects={simplifiedBookmarkedResourceObjects}
+                <UserProfile
+                    isOwnUser={isOwnUser}
+                    userID={userID}
+                    simplifiedCompletedResourceObjects={simplifiedCompletedResourceObjects}
+                    simplifiedBookmarkedResourceObjects={simplifiedBookmarkedResourceObjects}
 
-        />
+                />
 
-    </section>
+            </section>
 
 
-</div>
-  )
+        </div>
+    )
 }
 
 export default ProfilePage
--- src/components/shared/UserProfile.tsx
+++ src/components/shared/UserProfile.tsx
@@ -24,18 +24,25 @@ const UserProfile = ({isOwnUser, userID, simplifiedCompletedResourceObjects, sim
       completedRecords[paper.subject] = (completedRecords[paper.subject] || 0) + 1;
     });
 
+    const bookmarkedRecords: Record<string, number> = {};
+
+    bookmarkTableData?.forEach((paper) => {
+      bookmarkedRecords[paper.subject] = (bookmarkedRecords[paper.subject] || 0) + 1;
+    });
 
 
     return (
-    <div className="grid row-auto gap-2">
+    <div className="grid row-auto gap-4 md:gap-6">
 
-      <div className="bg-pri_bg_card rounded-xl px-2 py-2 md:px-4 flex_col_center w-full">
-            <h2 className="w-full px-2 text-md md:text-lg font-bold md:text-md text-pri_navy_dark text-start mb-2">{isOwnUser && "Your "}Completed Practices</h2>
+      <div className="bg-pri_bg_card rounded-xl px-2 py-2 md:px-4 grid grid-rows-auto lg:grid-cols-2 gap-2 w-full">
+        
+        <div className="col-span-1 row-span-1">
+            <h2 className="w-full px-2 text-md md:text-lg font-bold md:text-md text-pri_navy_darker text-center mb-2">{isOwnUser && "Your "}Completed Practices</h2>
             
             {Object.keys(completedRecords).length === 0 ?
                 <p className="w-full text-center italic text-pri_navy_main">0 completed papers found</p>
                 :
-                <ul className="w-full px-2 text-sm md:text-md md:text-md text-pri_navy_main">
+                <ul className="w-full px-4 lg:px-10 text-sm md:text-md md:text-md text-pri_navy_main">
                 {Object.entries(completedRecords).map(([subject, completions])=>{
                     return(
                         <li key={`${subject}_${completions}`} className="flex my-1">
@@ -46,6 +53,26 @@ const UserProfile = ({isOwnUser, userID, simplifiedCompletedResourceObjects, sim
                 })}
 
             </ul>}
+        </div>
+
+        <div className="col-span-1 row-span-1">
+            <h2 className="w-full px-2 text-md md:text-lg font-bold md:text-md text-pri_navy_darker text-center mb-2">{isOwnUser && "Your "}Bookmarks</h2>
+            
+            {Object.keys(bookmarkedRecords).length === 0 ?
+                <p className="w-full text-center italic text-pri_navy_main">0 bookmarks found</p>
+                :
+                <ul className="w-full px-4 lg:px-10 text-sm md:text-md md:text-md text-pri_navy_main">
+                {Object.entries(bookmarkedRecords).map(([subject, bookmarked])=>{
+                    return(
+                        <li key={`${subject}_${bookmarked}`} className="flex my-1">
+                            <p className="font-semibold">{subject}</p>
+                            <p className="font-bold ml-auto">{bookmarked} <span className="italic font-normal">bookmarked</span></p>
+                        </li>
+                    )
+                })}
+
+            </ul>}
+        </div>
 
         </div>
 
