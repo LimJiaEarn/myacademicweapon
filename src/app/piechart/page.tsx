aa6f19668bd8b93b6497d2613e804919d58f6114
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2024-04-22 15:42:32 +0800

    Refactored into Donut

--- src/app/piechart/page.tsx
+++ src/app/piechart/page.tsx
@@ -1,25 +1,29 @@
 
 import Pie from '@/components/shared/Piechart';
 
-const data = [10, 15, 20, 25, 30];
-const colors = ['#43A19E', '#7B43A1', '#F2317A', '#FF9824', '#58CF6C'];
-const radius = 150;
-const hole = 50;
-const strokeWidth = 3;
-
 const Page = () => {
-  return (
-    <div className="flex_center gap-4">
-      <h1>Pie Chart Example</h1>
-      <Pie
-        data={data}
-        colors={colors}
-        radius={radius}
-        hole={hole}
-        strokeWidth={strokeWidth}
-      />
-    </div>
-  );
+
+    const x = 20;
+    const data = [x, 100-x];
+    const colors = ['#43A19E', '#7B43A1'];
+    const radius = 150;
+    const hole = 100;
+    const strokeWidth = 4;
+
+    
+    return (
+      <div className="flex_center gap-4">
+
+
+        <Pie
+          data={data}
+          colors={colors}
+          radius={radius}
+          hole={hole}
+          strokeWidth={strokeWidth}
+        />
+      </div>
+    );
 };
 
 export default Page;
