fc57f532f25d82df61cd4d46bce4681cacb88674
Jia Earn <jiae4rn@gmail.com>
2024-05-01 17:58:39 +0800

    CSS touch ups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,13 @@ const HomePage = () => {
       </section>
 
 
-      <section className="w-full px-2 sm:px-4 md:px-6 mx-auto mb-[200px]">
+      <section className="w-full px-2 sm:px-4 md:px-6 mx-auto mb-[200px] flex_col_center">
+        <h1 className="text-2xl font-bold leading-tight text-pri_navy_dark sm:text-4xl sm:leading-normal md:text-5xl md:leading-relaxed">
+          Explore Our Features
+        </h1>
+        <p className="mt-6 mb-12 text-lg text-center leading-relaxed text-pri_navy_darker md:text-2xl max-w-[700px]">
+          We're more than just a collection of study resources, we're your steadfast ally in your pursuit of academic success!
+        </p>
         <Features/>
       </section>
 
--- src/components/shared/Features.tsx
+++ src/components/shared/Features.tsx
@@ -7,6 +7,7 @@ const Features = () => {
 
     return (
         <section className="grid mx-auto max-w-[1500px] auto-rows-min grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-8">
+
             <div className="bg-pink_bg p-4 md:p-6 rounded-2xl group col-span-1 lg:col-span-2 row-span-1 w-full h-full flex flex-col gap-4 text-pink_text">
                 <h2 className={`${baseHeadingStyle} `}>Set Goals</h2>
                 <p className={`${baseParaStyle} max-w-[450px]`}>Set your ambitious targets and don't lose sight of it! If you fail to plan, you plan to fail!</p>
@@ -31,7 +32,7 @@ const Features = () => {
                     <div className="relative  w-full flex justify-center items-center gap-2 min-h-[350px] lg:min-h-[450px]">
                         <div className="absolute h-[250px] w-[160px] lg:h-[320px] lg:w-[270px] xl:h-[400px] xl:w-[300px]">
                             <Image src="/images/feature2.1.png" alt="f2" fill={true}
-                                className="rounded-lg object-cover object-top -translate-x-8 -translate-y-2 -rotate-2 group-hover:rotate-4 group-hover:-translate-x-12 group-hover:-translate-y-12 duration-100 ease-in-out"/>
+                                className="rounded-lg object-cover object-top -translate-x-8 -translate-y-2 -rotate-2 group-hover:rotate-4 group-hover:-translate-x-8 lg:group-hover:-translate-x-12 group-hover:-translate-y-12 duration-100 ease-in-out"/>
                         </div>
                         <div className="absolute z-10 h-[250px] w-[160px] lg:h-[320px] lg:w-[270px] xl:h-[400px] xl:w-[300px]">
                             <Image src="/images/feature2.png" alt="f2" fill={true} quality={100}
@@ -48,7 +49,7 @@ const Features = () => {
                 <h2 className={`${baseHeadingStyle} col-span-full row-span-1`}>Unlimited Bookmarks</h2>
 
                 <Image src="/images/feature3.png" alt="f4" width={280} height={250} 
-                    className="rounded-lg mx-auto -translate-x-4 translate-y-2 -rotate-6 group-hover:-rotate-12 group-hover:-translate-x-6 group-hover:-translate-y-4 duration-100 ease-in-out"/>
+                    className="rounded-lg mx-auto -translate-x-4 translate-y-2 -rotate-6 group-hover:-rotate-12 group-hover:-translate-x-6 group-hover:-translate-y-4 group-hover:scale-[1.1] duration-200 ease-in-out"/>
 
                 <p className={`${baseParaStyle} `}>Bookmark your favourite resources and revisit them anytime you wish!</p>
             </div>
