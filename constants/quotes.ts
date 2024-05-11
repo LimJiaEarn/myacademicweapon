08f559e286ab5b5c9a68c9d70e5d3a6893454ee6
Jia Earn <jiae4rn@gmail.com>
2024-05-11 20:44:27 +0800

    More quotes

--- constants/quotes.ts
+++ constants/quotes.ts
@@ -3,9 +3,21 @@ interface SubjectQuote {
     writer?: string;
 }
 
-export const quotes : SubjectQuote[] = [
-    {quote: "The best preparation for tomorrow is doing your best today.", writer:"H. Jackson Brown Jr."},
-    {quote: "It’s not whether you get knocked down; it’s whether you get up.", writer: "Vince Lombardi"},
-    {quote: "The hardest arithmetic to master is that which enables us to count our blessings.", writer:"Eric Hoffer"},
-    {quote: "Not all treasure's silver and gold", writer:"Captain Jack Sparrow"},
-]
+export const quotes: SubjectQuote[] = [
+    { quote: "The best preparation for tomorrow is doing your best today.", writer: "H. Jackson Brown Jr." },
+    { quote: "It’s not whether you get knocked down; it’s whether you get up.", writer: "Vince Lombardi" },
+    { quote: "The hardest arithmetic to master is that which enables us to count our blessings.", writer: "Eric Hoffer" },
+    { quote: "Not all treasure's silver and gold", writer: "Captain Jack Sparrow" },
+    { quote: "The problem is not the problem. The problem is your attitude about the problem.", writer: "Captain Jack Sparrow" },
+    { quote: "The secret of getting ahead is getting started.", writer: "Mark Twain" },
+    { quote: "You don’t have to be great to start, but you have to start to be great.", writer: "Zig Ziglar" },
+    { quote: "Don’t watch the clock; do what it does. Keep going.", writer: "Sam Levenson" },
+    { quote: "Don’t let what you cannot do interfere with what you can do.", writer: "John Wooden" },
+    { quote: "The future depends on what you do today.", writer: "Mahatma Gandhi" },
+    { quote: "The harder you work for something, the greater you’ll feel when you achieve it.", writer: "Anonymous" },
+    { quote: "You miss 100% of the shots you don’t take.", writer: "Wayne Gretzky" },
+    { quote: "Winners don't make excuses when the other side plays the game.", writer: "Harvey Specter" },
+    { quote: "The bad news is time flies. The good news is you're the pilot", writer: "Michael Altshuler" },
+    { quote: "The recognition that there is a problem, is the beginning of a solution", writer: "Lee Kuan Yew" },
+    { quote: "If you look at the people in your circle and you don't get inspired, then you don't have a circle. You have a cage.", writer: "Nipsey Hussle" },
+];
